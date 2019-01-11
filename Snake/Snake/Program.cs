aeedab37c1e3097af6bfd6c292113b4a2865c741
AdlerMD <adler.airsoft@gmail.com>
2019-01-11 17:35:18 +0200

    Созданн класс Figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,16 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            // Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            verticallines leftLine = new verticallines(0, 24, 0, '+');
+            verticallines rightLine = new verticallines(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
-
-            Console.ReadLine();
+            // Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
         }
     }
 }
+       
